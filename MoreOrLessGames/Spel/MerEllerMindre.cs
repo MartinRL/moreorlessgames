7412db32ac87693c118e7c854cf7ad9fa7a0e090
Martin Rosén-Lidholm <martin.rosenlidholm@gmail.com>
2024-10-07 18:35:13 +0200

    en början...

---
+++ MoreOrLessGames/Spel/MerEllerMindre.cs
@@ -0,0 +1,23 @@
+﻿namespace MoreOrLessGames.Spel;
+
+using Händelser = IReadOnlyCollection<IHändelse>;
+
+// händelser
+public interface IHändelse { } // used to mimic a discriminated union
+
+// kommandon
+public interface IKommando { }
+
+// vyer
+public interface IVy { }
+
+public record Tillstånd(decimal Balance, bool IsClosed)
+{
+    public static readonly Tillstånd Initialt = new(0, false);
+}
+
+public record Spelare(byte id, string name);
+
+public static class MerEllerMindre
+{
+}
