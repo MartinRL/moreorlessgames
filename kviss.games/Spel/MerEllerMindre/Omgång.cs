8f115c8e5cabfd2fb62fd4aee60a44634153c9ce
Martin Rosén-Lidholm <martin.rosenlidholm@gmail.com>
2024-10-09 15:12:32 +0200

    första :red_circle: testen :neutral_face:

--- kviss.games/Spel/MerEllerMindre/Omgång.cs
+++ kviss.games/Spel/MerEllerMindre/Omgång.cs
@@ -1,21 +1,53 @@
 ﻿namespace kviss.games.Spel.MerEllerMindre.Omgång;
 
 using Händelser = IReadOnlyCollection<IHändelse>;
-using Frågor = IReadOnlyCollection<Fråga>;
+using static System.Guid;
+using Frågor = ISet<Fråga>;
+using Microsoft.Extensions.Logging;
+
+public record Spelare(string Namn);
 
 public record Fråga(string Kategori, string Jämförelse, string Enhet, string Jämförelsefråga, string Deltafråga, string Alt1Namn, int Alt1Tal, string Alt2Namn, int Alt2Tal);
 
 // händelser
 public interface IHändelse { } // used to mimic a discriminated union
-public record Startad(ushort Id, Spelare Spelmästare, Frågor Frågor) : IHändelse;
+public record Skapad : IHändelse
+{
+    public Guid Id { get; init; } = NewGuid();
+    public Spelare Spelmästare { get; init; }
+    public Frågor Frågor { get; init; }
+
+    public Skapad(Spelare spelmästare, Frågor frågor) { Spelmästare = spelmästare; Frågor = frågor; }
+}
 
 // kommandon
 public interface IKommando { }
-public record Starta(Spelare Spelmästare, Frågor Frågor /* antal frågor = omgångens längd */ ) : IKommando;
+public record Skapa(Spelare Spelmästare, Frågor Frågor /* antal frågor = omgångens längd */ ) : IKommando;
 
 // vyer
 public interface IVy { }
 
-public record Tillstånd(decimal Balance, bool ÄrStartad, bool ÄrAvslutad);
+public record Tillstånd(IDictionary<string, ushort> Ställning, bool ÄrStartad, bool ÄrAvslutad)
+{
+    public static readonly Tillstånd Initialt = new(new Dictionary<string, ushort>(), false, false);
+}
+public static class Beslutare
+{
+    public static Händelser Besluta(this Tillstånd @this, IKommando kommando) =>
+        kommando switch
+        {
+            Skapa k => Skapa(k),
+            /*Withdraw k => Withdraw(c),
+            Close k => Close(state, c),*/
+            _ => throw new InvalidOperationException()
+        };
+    private static Händelser Skapa(Skapa k) =>
+        new Skapad(k.Spelmästare, k.Frågor).ToArray();
+}
 
-public record Spelare(string Namn);
+// hjälpfunktioner
+
+public static class Extensions
+{
+    public static Händelser ToArray(this IHändelse h) => [h];
+}
---
+++ kviss.games/Spel/MerEllerMindre/Tester.cs
@@ -0,0 +1,18 @@
+﻿namespace kviss.games.Spel.MerEllerMindre.Omgång;
+
+using System;
+using FluentAssertions;
+using Xunit;
+using Frågor = ISet<Fråga>;
+
+public class OmgångSkall
+{
+    public readonly Frågor EnFråga = new HashSet<Fråga>() { new Fråga("musik", "flest", "miljoner", "Vilken grupp har sålt flest album, {alt1_namn} eller {alt2_namn}?", "{alt1_namn} har sålt flest album, {alt1_tal} { enhet}. Hur många färre {enhet} album har { alt2_namn } sålt?", "ABBA", 400, "Roxette", 75)};
+
+    [Fact]
+    public void Startas() =>
+        Tillstånd.Initialt
+        .Besluta(new Skapa(new Spelare("Martin"), EnFråga))
+        .Should()
+        .Equal(new Skapad(new Spelare("Martin"), EnFråga).ToArray());
+}
