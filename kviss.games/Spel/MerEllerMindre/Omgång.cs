1ccb07f58f0e1f6d511962693feba8aa90bf2efe
Martin Rosén-Lidholm <martin.rosenlidholm@gmail.com>
2024-10-09 09:33:00 +0200

    mellansteg

--- kviss.games/Spel/MerEllerMindre/Omgång.cs
+++ kviss.games/Spel/MerEllerMindre/Omgång.cs
@@ -1,12 +1,17 @@
 ﻿namespace kviss.games.Spel.MerEllerMindre.Omgång;
 
 using Händelser = IReadOnlyCollection<IHändelse>;
+using Frågor = IReadOnlyCollection<Fråga>;
+
+public record Fråga(string Kategori, string Jämförelse, string Enhet, string Jämförelsefråga, string Deltafråga, string Alt1Namn, int Alt1Tal, string Alt2Namn, int Alt2Tal);
 
 // händelser
 public interface IHändelse { } // used to mimic a discriminated union
+public record Startad(ushort Id, Spelare Spelmästare, Frågor Frågor) : IHändelse;
 
 // kommandon
 public interface IKommando { }
+public record Starta(Spelare Spelmästare, Frågor Frågor) : IKommando;
 
 // vyer
 public interface IVy { }
